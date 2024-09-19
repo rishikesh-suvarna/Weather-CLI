8dbadb92c57be2081af10c5d9d3c2d5d034a0915
Rishikesh Suvarna <rishikeshsuvarna@gmail.com>
2024-09-19 02:32:33 +0100

    feat: Add .gitignore and forecast.json files
    feat: Fetched data from weather API and displayed in a hourly format

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rishikeshsuvarna/weather-cli
 
 go 1.22.5
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,95 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Current struct {
+		TempC     float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch int64   `json:"time_epoch"`
+				TempC     float64 `json:"temp_c"`
+				Condition struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+				ChanceOfRain float64 `json:"chance_of_rain"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
+}
 
 func main() {
-	fmt.Println("Project Init")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	apiKey := os.Getenv("WEATHER_API_KEY")
+
+	if apiKey == "" {
+		panic("WEATHER_API_KEY is not set")
+	}
+
+	res, err := http.Get("https://api.weatherapi.com/v1/forecast.json?q=London&days=1&key=" + apiKey)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic("Weather API error / Not available")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+
+	location, current, hours := weather.Location, weather.Current, weather.Forecast.Forecastday[0].Hour
+
+	fmt.Printf("Location: %s, %s, %.0fC %s\n", location.Name, location.Country, current.TempC, current.Condition.Text)
+
+	for _, hour := range hours {
+
+		date := time.Unix(hour.TimeEpoch, 0)
+
+		if date.Before(time.Now()) {
+			continue
+		}
+
+		message := fmt.Sprintf("Time: %s, %.0fC %s, %.0f%% chance of rain",
+			date.Format("15:04"),
+			hour.TempC,
+			hour.Condition.Text,
+			hour.ChanceOfRain)
+
+		fmt.Println(message)
+	}
+
 }
